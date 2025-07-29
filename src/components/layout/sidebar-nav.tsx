e023ada8d90db343446edca2125e7bb2a3a50dfe
Subhasish Nayak <subhasishnayak38@gmail.com>
2025-07-29 20:28:05 +0000

    ok if firebase is more good then use it but keep in mind  that i wantt t

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -30,9 +30,8 @@ export function SidebarNav() {
         <>
           {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} passHref>
+              <Link href={item.href}>
                 <SidebarMenuButton
-                  as="a"
                   isActive={pathname === item.href}
                   tooltip={{ children: item.label, side: "right", align: "center" }}
                 >
